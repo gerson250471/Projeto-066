851ba13151882729b6f6e10438522a5506ee9a98
Gerson Bernardo da Silva <gbds25@gmail.com>
2023-02-08 13:20:36 -0300

    Correções na parte de Arquiar e Gerar Demanda

--- Arquivar.js
+++ Arquivar.js
@@ -13,25 +13,26 @@ function arquivar() {
   let r2 = aba2.getRange("B2").getValue();
 
   for (var  a = 0; a < 17; a ++){
-    if (rng1[a][0] != "") {
-      aba3.getRange(arqLf,1).setValue(rng1[a][0]);
-      aba3.getRange(arqLf,2).setValue(r1);
-      aba3.getRange(arqLf,3).setValue(rng1[a][5]);
-      aba3.getRange(arqLf,4).setValue(rng1[a][6]);
-      aba3.getRange(arqLf,5).setValue(rng1[a][7]);
-      if (rng1[a][7]!=""){
-        aba3.getRange(arqLf,9).setValue(1);
-      }else{
-        aba3.getRange(arqLf,9).setValue(0);
-      }
+        if (rng1[a][0] != "") {
+        aba3.getRange(arqLf,1).setValue(rng1[a][0]);
+        aba3.getRange(arqLf,2).setValue(r1);
+        aba3.getRange(arqLf,3).setValue(rng1[a][5]);
+        aba3.getRange(arqLf,4).setValue(rng1[a][6]);
+        aba3.getRange(arqLf,5).setValue(rng1[a][7]);
+        if (rng1[a][7]!=""){
+            aba3.getRange(arqLf,9).setValue(1);
+        }else{
+            aba3.getRange(arqLf,9).setValue(0);
+        }
 
-      var dt =  new Date(rng1[a][6]);
-      var dt1=dt.getMonth()+1;
-      var dt2=dt.getFullYear();
-    
-      aba3.getRange(arqLf,6).setFormula('=WEEKNUM(R[0]C[-2])');
-      aba3.getRange(arqLf,7).setValue(dt1);
-      aba3.getRange(arqLf,8).setValue(dt2);
-      arqLf = aba3.getLastRow()+1;
+        var dt =  new Date(rng1[a][6]);
+        var dt1=dt.getMonth()+1;
+        var dt2=dt.getFullYear();
+        
+        aba3.getRange(arqLf,6).setFormula('=WEEKNUM(R[0]C[-2])');
+        aba3.getRange(arqLf,7).setValue(dt1);
+        aba3.getRange(arqLf,8).setValue(dt2);
+        arqLf = aba3.getLastRow()+1;
+        }
     }
 }
